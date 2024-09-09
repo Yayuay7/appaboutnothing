67aff80bf3c3dd95545e169cff1876c6f5933c5c
Yayuay7 <113195225+Yayuay7@users.noreply.github.com>
2024-09-09 08:20:31 -0400

    Initial Commit

---
+++ appaboutnothing/appaboutnothingApp.swift
@@ -0,0 +1,32 @@
+//
+//  appaboutnothingApp.swift
+//  appaboutnothing
+//
+//  Created by Jayden Cheris on 9/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct appaboutnothingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
